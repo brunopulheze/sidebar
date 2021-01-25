54c25b5340420d13babf2483f19f1ec52d35fa2f
brunopulheze <brunopulheze@hotmail.com>
2021-01-25 15:59:44 +0100

    sidebar

---
+++ main.js
@@ -0,0 +1,7 @@
+function showList() {
+    document.getElementById('sublist').style.display = 'block';
+}
+
+function showSecondList() {
+    document.getElementById('second-sublist').style.display = 'block';
+}
